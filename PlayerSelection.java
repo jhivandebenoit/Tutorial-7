b51711355765389266b508e39b774e55f38aaad5
Jhivan de Benoit <jhivanb@gmail.com>
2021-07-08 11:37:56 +0530

    add player and view player methods

--- PlayerSelection.java
+++ PlayerSelection.java
@@ -1,4 +1,36 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PlayerSelection {
-    private String[] players;
-    
+    private ArrayList<String> players = new ArrayList<String>();
+    private Scanner scanner = new Scanner(System.in);
+
+    public void menu() {
+
+        String input;
+        System.out.println("Enter code");
+        input = scanner.nextLine();
+        while (true) {
+            switch(input.toUpperCase()) {
+                case "A":
+                    addPlayer();
+                    break;
+                case "V":
+                    viewPlayers();
+                    break;
+            }
+        }
+
+    }
+    public void addPlayer() {
+        System.out.println("Enter player name: ");
+        players.add(scanner.nextLine());
+
+
+    }
+    public void viewPlayers() {
+        System.out.println(players);
+
+    }
+
 }
